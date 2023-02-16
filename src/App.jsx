70ae6bba0781037cdf4bcbf9eed903cc7afbb24a
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-02-16 08:36:56 -0300

    Correção em no nome da page 404

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Cadastro from "./pages/Cadastro"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
-import Page404 from "./pages/page404"
+import Page404 from "./pages/Page404"
 import Redefinir from "./pages/Redefinir"
 import GlobalStyles from "./styles/GlobalStyles"
 
