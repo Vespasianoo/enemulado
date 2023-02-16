65ea22c0e2521692a4177a073c094a451520de73
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-02-16 11:08:02 -0300

    rotas que não são encontradas

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/redefinir" element={<Redefinir />} />
-          <Route path="/*" element={<Page404 />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
       </BrowserRouter>
       <GlobalStyles />
