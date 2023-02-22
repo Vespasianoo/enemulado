5cf926656558ddab769d185ef78c541f36dd9865
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-02-22 20:29:15 -0300

    rotas privadas

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Login from "./pages/Login"
 import Page404 from "./pages/Page404"
 import Redefinir from "./pages/Redefinir"
 import Simulados from "./pages/Simulados"
+
+import PrivateRoute from "./routes/PrivateRoute"
 import GlobalStyles from "./styles/GlobalStyles"
 
 function App() {
@@ -16,7 +18,14 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/redefinir" element={<Redefinir />} />
-          <Route path="/simulados" element={<Simulados />} />
+          <Route
+            path="/simulados"
+            element={
+              <PrivateRoute>
+                <Simulados />
+              </PrivateRoute>
+            }
+          />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </BrowserRouter>
