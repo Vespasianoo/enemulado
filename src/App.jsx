e0e752f52e483b6a9f6e82a2e5e44d1e416f4c6f
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-02-24 14:01:05 -0300

    retirando page de cadastro entrar com Google

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/cadastro" element={<Cadastro />} />
+          {/* <Route path="/cadastro" element={<Cadastro />} /> */}
           <Route path="/redefinir" element={<Redefinir />} />
           <Route
             path="/simulados"
